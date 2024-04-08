0b643b608590d2a9f3da3ad1c8c0dcf82c792ab1
Ba-iitso <145634098+Ba-iitso@users.noreply.github.com>
2024-04-08 08:37:04 -0700

    Added mini.nvim plugins; Around/inside, surround, statusline.

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -250,6 +250,40 @@ require('lazy').setup({
 	    vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
         end
     },
+    
+    {--Collection of various small independant plugins/modules
+        'echasnovsky/mini.nvim',
+        config = function()
+            --Better Around/Inside textobjects
+            --
+            --Examples:
+            -- - va)  - [V]isually select [A]round [)]parenthen
+            -- - yinq - [Y]ank [I]nside [N]ext [']quote
+            -- - ci'  - [C]hange [I]nside [']quote
+            require('mini.ai').setup { n_lines = 500 }
 
+            -- Add/delete/replace surroundings (brackets, quotes, etc)
+            --
+            -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+            -- - sd'   - [S]urround [D]elete [']quotes
+            -- - sr)'  - [S]urround [R]eplace [)] [']
+            require('mini.surround').setup()
+
+            -- Simple easy statusline
+            local statusline = require 'mini.statusline'
+            statusline.setup()
+
+            -- You can configure sections in the statusline by overriding their
+            -- default behavior. For example, here we disable the section for
+            -- cursor information because line numbers are already enabled
+            ---@diagnostic disable-next-line: duplicate-set-field
+            statusline.section_location = function()
+                return ''
+            end
+
+            -- ... and there is more!
+            -- Check out: https://github.com/echasnovsky/mini.nvim
+        end,
+    },
 
 }, {})
