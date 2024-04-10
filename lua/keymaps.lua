6e84815fb3b84af12d0ae37b0aabdec505326681
Ba-iitso <145634098+Ba-iitso@users.noreply.github.com>
2024-04-09 18:58:18 -0700

    moved lsp attatch settings to lazy-plugins

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -20,27 +20,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '>-2<CR>gv=gv")
 
--- Commands only map after language server attaches to current buffer
-vim.api.nvim_create_autocmd('LspAttach', {
-    group = BaiitsoGroup,
-    callback = function(ev)
-	-- Buffer local mappings
-	-- See ':help vim.lsp.*'
-	local opts = { buffer = ev.buf }
-	vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition(), opts)
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition(), opts)
-	vim.keymap.set("n", "gD", vim.lsp.buf.declaration(), opts)
-	vim.keymap.set("n", "gi", vim.lsp.buf.implementation(), opts)
-	vim.keymap.set("n", "gr", vim.lsp.buf.references(), opts)
-	vim.keymap.set("n", "K", vim.lsp.buf.hover(), opts)
-	vim.keymap.set("n", "<leader>vd", vim.lsp.diagnostic.open_float())
-	vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol(), opts)
-	vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action(), opts)
-	vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename(), opts)
-	vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help(), opts)
-    end,
-})
-
 -- Diagnostics
 vim.diagnostic.config({
     virtual_text = true,
@@ -54,6 +33,10 @@ vim.diagnostic.config({
 	prefix = '',
     },
 })
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Execute current file if executable for linux
 vim.keymap.set("n", "<leader>f.", function()
